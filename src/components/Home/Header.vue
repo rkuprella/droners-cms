d9347f36535ee2c0b01c5cca43cb8916db30e2b4
Quabbe <robinkuprella@gmail.com>
2019-06-09 03:56:01 +0200

    Edits

--- src/components/Home/Header.vue
+++ src/components/Home/Header.vue
@@ -1,6 +1,30 @@
 <template>
-  <div class="header"></div>
+  <div class="header">
+    <div class="container">
+      <div class="title-wrapper">hgi</div>
+      <div class="menu-wrapper">asd</div>
+    </div>
+  </div>
 </template>
 
 <style>
+.header {
+  height: 100vh;
+  min-height: 600px;
+  display: flex;
+  align-items: center;
+}
+.title-wrapper {
+  margin-top: 20vh;
+  width: 100%;
+  height: 40vh;
+  min-height: 200px;
+  border: 2px solid red;
+}
+.menu-wrapper {
+  width: 100%;
+  height: 20vh;
+  min-height: 40px;
+  border: 2px solid blue;
+}
 </style>
